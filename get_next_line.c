b528f7b6e6d855403e03bbd8d1243ed0fddd4f0a
jsimeli0 <69462144+jsimeli0@users.noreply.github.com>
2021-01-09 00:55:46 +0100

    First version that works! Doesn't work with empty lines though.

--- get_next_line.c
+++ get_next_line.c
@@ -6,25 +6,25 @@
 /*   By: jsimelio <jsimelio@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/12/14 12:42:51 by jsimelio      #+#    #+#                 */
-/*   Updated: 2021/01/09 00:03:52 by jsimelio      ########   odam.nl         */
+/*   Updated: 2021/01/09 00:53:06 by jsimelio      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
 /* Checks for a whole line in buff_static and saves it to *line */
-int	pop(char *buff_static, char **line)
+int	pop(char **buff_static, char **line)
 {
 	int			cut;
 	int			len;
 
-	if ((cut = ft_strchr_int(buff_static, '\n')) != -1)
+	if ((cut = ft_strchr_int(*buff_static, '\n')) != -1)
 	{
-		*line = ft_substr(buff_static, 0, cut);
+		*line = ft_substr(*buff_static, 0, cut);
 		if (*line == NULL)
 			return (-1);
-		len = ft_strlen(buff_static) - cut;
-		buff_static = ft_substr(buff_static, cut, len);
+		len = ft_strlen(*buff_static) - cut;
+		*buff_static = ft_substr(*buff_static, cut, len);
 		return (1);
 	}
 	return (0);
@@ -40,23 +40,22 @@ int	get_next_line(int fd, char **line)
 	// if ((fd < 0 || !line || read(fd, buffer, 0) < 0))
 	// 	return (-1);
 	/* Pop a line from buff_static - if available - and do a NULL-check */
-	if (buff_static)	
-		if (pop(buff_static, line))
+	while ((read_return = read(fd, buff_read, BUFFER_SIZE)) > 0)
+	{
+		buff_read[BUFFER_SIZE] = 0;
+		buff_static = ft_strjoin(buff_static, buff_read);
+		if (buff_static == NULL)
+			return (-1);
+		if (pop(&buff_static, line))
 			return (1);
-	read_return = read(fd, buff_read, BUFFER_SIZE);
-	if (read_return < 0)
-		return (-1);
-	else if (read_return == 0)
+	}
+	if (read_return == 0)
 	{
 		*line = ft_strdup("");
 		return (0);
 	}
-	buff_read[BUFFER_SIZE] = 0;
-	buff_static = ft_strjoin(buff_static, buff_read);
-	if (buff_static == NULL)
-		return (-1);
-	get_next_line(fd, line);
-	return (1);
+	return (-1);
+	
 	// if (read_return == BUFFER_SIZE)
 	// {
 	// 	get_next_line(fd, line);
